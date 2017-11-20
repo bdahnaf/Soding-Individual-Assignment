ac90e9a14e786a525f9079e8097a5be9b0fafdc3
Ahnaf Shahriar <bdahnaf@gmail.com>
2017-11-20 15:17:20 +0600

    Add files via upload
    The complete solution can be run using these files.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace SodingIndividualAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNumber = 0;
+            int secondNumber = 1;
+            int thirdNumber = 0;
+            int sum = 0;
+            int i = 0;
+            Console.Write("Even Fibonacci Series under four million(4000000)");
+            while (thirdNumber < 4000000)
+            {
+                thirdNumber = firstNumber + secondNumber;
+                if (thirdNumber % 2 == 0)
+                {
+                    Console.Write(thirdNumber + " ");
+                    sum = sum + thirdNumber;
+                }
+                firstNumber = secondNumber;
+                secondNumber = thirdNumber;
+                i++;
+            }
+            Console.WriteLine();
+            Console.WriteLine("Summation of the series=" + sum);
+            Console.ReadKey();
+        }
+    }
+}
